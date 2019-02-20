1f83d373496557117c7ac28a4af4b19874ef8a14
Sergey Trufanov <itrufik@gmail.com>
2019-02-20 16:55:03 +0200

    TextViewExtensions -> StringExtensions, add few more extension to string, add documentation

---
+++ kotlinandroidextensions/src/main/java/com/wearebrain/kotlinandroidextensions/StringExtensions.kt
@@ -0,0 +1,53 @@
+package com.wearebrain.kotlinandroidextensions
+
+import android.os.Build
+import android.text.Html
+import android.util.Patterns
+
+/**
+ * Returns empty string -> ""
+ */
+val String.Companion.EMPTY: String
+    get() = ""
+
+/**
+ * Returns whitespace string -> " "
+ */
+val String.Companion.WHITESPACE: String
+    get() = " "
+
+/**
+ * Clear all HTML tags from this string.
+ *
+ * @see Html.fromHtml for Build.VERSION >= Build.VERSION_CODES.N
+ * @see Html.fromHtml otherwise
+ */
+fun String.clearHtml(): String {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY).toString()
+    } else {
+        Html.fromHtml(this).toString()
+    }
+}
+
+/**
+ * Check if this string is valid web url
+ *
+ * @see Patterns.WEB_URL
+ */
+fun String.isValidWebUrl() = Patterns.WEB_URL.matcher(this).matches()
+
+/**
+ * Check if this string is valid web url
+ *
+ * @see Patterns.EMAIL_ADDRESS
+ */
+fun String.isValidEmail() = Patterns.EMAIL_ADDRESS.matcher(this).matches()
+
+/**
+ * Check if this string is valid web url
+ *
+ * @see Patterns.PHONE
+ */
+fun String.isValidPhoneNumber() = Patterns.PHONE.matcher(this).matches()
+
