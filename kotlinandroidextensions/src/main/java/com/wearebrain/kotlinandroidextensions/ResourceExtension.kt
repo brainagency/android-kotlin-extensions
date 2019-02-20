2c4b236e52f84743e6a49c1012665a443d5c2c05
Sergey Trufanov <itrufik@gmail.com>
2019-02-20 16:45:55 +0200

    Added documentation for ResourceExtensiong

--- kotlinandroidextensions/src/main/java/com/wearebrain/kotlinandroidextensions/ResourceExtension.kt
+++ kotlinandroidextensions/src/main/java/com/wearebrain/kotlinandroidextensions/ResourceExtension.kt
@@ -2,14 +2,26 @@ package com.wearebrain.kotlinandroidextensions
 
 import android.content.res.Resources
 
+/**
+ * Converts value to dp analog according to phones density
+ */
 val Int.dp: Int
     get() = (this / Resources.getSystem().displayMetrics.density).toInt()
 
+/**
+ * Converts value to px analog according to phones density
+ */
 val Int.px: Int
     get() = (this * Resources.getSystem().displayMetrics.density).toInt()
 
+/**
+ * Converts value to dp analog according to phones density
+ */
 val Float.dp: Float
     get() = this / Resources.getSystem().displayMetrics.density
 
+/**
+ * Converts value to px analog according to phones density
+ */
 val Float.px: Float
     get() = this * Resources.getSystem().displayMetrics.density
